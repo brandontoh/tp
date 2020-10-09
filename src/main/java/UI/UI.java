4a0d697e9bd17deabc7f2c6196e631107c0c7378
brandontoh <brandontoh@yahoo.com.sg>
2020-10-09 12:53:36 +0800

    Add Initial UI file

---
+++ src/main/java/UI/UI.java
@@ -0,0 +1,39 @@
+package UI;
+
+import java.util.Scanner;
+
+public class UI {
+
+    public UI() {
+
+    }
+
+    public void displayWelcomeScreen() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+    }
+
+    public String askForUserInput() {
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        return userInput;
+    }
+
+    public void printExitMessage() {
+        System.out.println("Bye Friend");
+    }
+
+    public void printCheatSheet() {
+        int index = 0;
+        String cs = "";
+        System.out.println(index + ": " + cs);
+    }
+    public int hello(int testing) {
+        return testing;
+    }
+}
---
+++ src/test/java/UI/UITest.java
@@ -0,0 +1,26 @@
+package UI;
+
+import org.junit.jupiter.api.Test;
+
+class UITest {
+
+    @Test
+    public void displayWelcomeScreen() {
+    }
+
+    @Test
+    void askForUserInput() {
+    }
+
+    @Test
+    void printExitMessage() {
+    }
+
+    @Test
+    void printCheatSheet() {
+    }
+
+    @Test
+    void hello() {
+    }
+}
