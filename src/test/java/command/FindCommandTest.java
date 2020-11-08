7036d906914475c46d35ec23bc2545782b63f1ec
brandontoh <brandontoh@yahoo.com.sg>
2020-11-08 23:15:43 +0800

    Add findCommandTest

--- src/test/java/command/FindCommandTest.java
+++ src/test/java/command/FindCommandTest.java
@@ -2,46 +2,30 @@ package command;
 
 import cheatsheet.CheatSheet;
 import cheatsheet.CheatSheetList;
+import editor.Editor;
 import exception.CommandException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import parser.Parser;
+import ui.Printer;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class FindCommandTest {
-    /*
-    @Test
-    void execute_validInput_success() {
-        final String userInput = "/find /k Details";
-        CheatSheetList.clear();
-        for (int i = 0; i < 10; i++) {
-            CheatSheetList.add(new CheatSheet("Name" + i, "Language" + i, "Details" + i));
-        }
-        try {
-            Parser parser = new Parser();
-            FindCommand findCommand = new FindCommand(parser);
-            findCommand.execute();
-        } catch (CommandException e) {
-            fail();
-        }
-    }
-    */
 
-//    @Test
-//    void execute_noArgument_exceptionThrown() {
-//        final String userInput = "/find";
-//        CheatSheetList.clear();
-//        for (int i = 0; i < 10; i++) {
-//            CheatSheetList.add(new CheatSheet("Name" + i, "Language" + i, "Details" + i));
-//        }
-//        try {
-//            Parser parser = new Parser();
-//            Command findCommand = parser.parse(userInput);
-//            findCommand.execute();
-//            fail();
-//        } catch (CommandException e) {
-//            assertEquals("Please enter at least an argument", e.getMessage());
-//        }
-//    }
+    @Test
+    void execute_nameAndSubjectFilled_success() throws CommandException, IOException, InterruptedException {
+        CheatSheetList cheatSheetList = new CheatSheetList();
+        AddCommandStub addCommandStub = new AddCommandStub(new Printer(), cheatSheetList, new Editor());
+        addCommandStub.populateFlagsToDescription("FirstTest", "Java");
+        addCommandStub.executeStub("Content1");
+        Assertions.assertAll(
+            () -> assertEquals("FirstTest", cheatSheetList.get(1).getName()),
+            () -> assertEquals("Java", cheatSheetList.get(1).getSubject()),
+            () -> assertEquals("Content1", cheatSheetList.get(1).getDetails())
+        );
+    }
 }
