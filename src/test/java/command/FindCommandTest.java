d3b96777e86aaf7a60689c2f1e0f232510e1af24
brandontoh <brandontoh@yahoo.com.sg>
2020-11-09 14:53:52 +0800

    Junit with stubs

--- src/test/java/command/FindCommandTest.java
+++ src/test/java/command/FindCommandTest.java
@@ -4,12 +4,44 @@ import cheatsheet.CheatSheet;
 import cheatsheet.CheatSheetList;
 import exception.CommandException;
 import org.junit.jupiter.api.Test;
+import parser.CommandFlag;
 import parser.Parser;
+import stubs.EditorStub;
+import ui.Printer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class FindCommandTest {
+
+    @Test
+    void execute_validName_success() throws CommandException {
+        CheatSheetList cheatSheetList = new CheatSheetList();
+        EditorStub editorStub = new EditorStub();
+
+        AddCommand addCommand = new AddCommand(new Printer(), cheatSheetList, editorStub);
+        HashMap<CommandFlag, String> addCommandMap = new HashMap<>();
+        addCommandMap.put(CommandFlag.NAME, "Test1");
+        addCommandMap.put(CommandFlag.SUBJECT, "Java1");
+        addCommand.setFlagsToDescriptionsMap(addCommandMap);
+        editorStub.writeContent("Content1");
+        addCommand.execute();
+
+        FindCommand findCommand = new FindCommand(new Printer(), cheatSheetList);
+        HashMap<CommandFlag, String> findCommandMap = new HashMap<>();
+        findCommandMap.put(CommandFlag.NAME, "Test1");
+        findCommand.setFlagsToDescriptionsMap(findCommandMap);
+        findCommand.execute();
+
+        String data = "Hello, World!\r\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+    }
+
     /*
     @Test
     void execute_validInput_success() {
---
+++ src/test/java/stubs/EditorStub.java
@@ -0,0 +1,33 @@
+package stubs;
+
+import cheatsheet.CheatSheet;
+import editor.Editor;
+import exception.EditorException;
+
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+
+public class EditorStub extends Editor {
+    private String content;
+
+    public void setContent(CheatSheet cheatSheet) {
+        content = cheatSheet.getDetails();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void writeContent(String text) {
+        content = text;
+    }
+
+    public void setEditingContentAttributes(String name, String subject) {
+    }
+
+    public void open() {
+    }
+
+    public void waitForClose() {
+    }
+}
